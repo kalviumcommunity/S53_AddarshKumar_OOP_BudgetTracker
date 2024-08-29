7c7911c655f6f782f00489e9ce3fd37f2133a642
AddarshKumar123 <addarshkumar2005@gmail.com>
2024-08-29 16:04:12 +0530

    added classes and objects

---
+++ User.java
@@ -0,0 +1,52 @@
+public class User {
+    private String userName;
+    private String password;
+
+    public String name;
+    public int age;
+    public String occupation;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+}
+
+
+
