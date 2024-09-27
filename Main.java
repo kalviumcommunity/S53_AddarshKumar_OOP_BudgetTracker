cc89e62333aea8882657d6e02434fb5425861a9c
AddarshKumar123 <addarshkumar2005@gmail.com>
2024-09-27 17:11:12 +0530

    Added static member function in Transaction class

--- Main.java
+++ Main.java
@@ -3,47 +3,48 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<User> users = new ArrayList<>(Arrays.asList(
-            new User("Addarsh_123","12345a","Addarsh",19,"Software Engineer"),
-            new User("Mukund_123","67623b","Mukund",30,"Python developer"),
-            new User("Himanshu_123","138793c","Himanshu",12,"Data Analyst"),
-            new User("Aditya_123","83298d","Aditya",60,"UI Researcher")
-        ));
-        User user1 = new User();
+//        ArrayList<User> users = new ArrayList<>(Arrays.asList(
+//            new User("Addarsh_123","12345a","Addarsh",19,"Software Engineer"),
+//            new User("Mukund_123","67623b","Mukund",30,"Python developer"),
+//            new User("Himanshu_123","138793c","Himanshu",12,"Data Analyst"),
+//            new User("Aditya_123","83298d","Aditya",60,"UI Researcher")
+//        ));
+//        User user1 = new User();
+//
+//        Scanner sc =new Scanner(System.in);
+//        System.out.print("Enter username ");
+//        String username=sc.nextLine();
+//
+//        System.out.println("Enter Password");
+//        String password=sc.nextLine();
+//
+//        System.out.println("Enter name");
+//        String name=sc.nextLine();
+//
+//        System.out.println("Enter age");
+//        int age=sc.nextInt();
+//        sc.nextLine();
+//
+//        System.out.println("Enter your occupation");
+//        String occupation=sc.nextLine();
+//        System.out.println();
+//
+//        sc.close();
+//
+//        user1.setUserName(username);
+//        user1.setPassword(password);
+//        user1.setName(name);
+//        user1.setAge(age);
+//        user1.setOccupation(occupation);
+//
+//
+//        users.add(user1);
+//
+//        for(int i=0;i<users.size();i++){
+//            System.out.println(users.get(i).getName());
+//        }
 
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter username ");
-        String username=sc.nextLine();
-
-        System.out.println("Enter Password");
-        String password=sc.nextLine();
-
-        System.out.println("Enter name");
-        String name=sc.nextLine();
-
-        System.out.println("Enter age");
-        int age=sc.nextInt();
-        sc.nextLine();
-
-        System.out.println("Enter your occupation");
-        String occupation=sc.nextLine();
-        System.out.println();
-
-        sc.close();
-
-        user1.setUserName(username);
-        user1.setPassword(password);
-        user1.setName(name);
-        user1.setAge(age);
-        user1.setOccupation(occupation);
-
-
-        users.add(user1);
-
-        for(int i=0;i<users.size();i++){
-            System.out.println(users.get(i).getName());
-        }
-
+        Scanner sc=new Scanner(System.in);
         for(int i=0;i<3;i++){
         String date=sc.nextLine();
         int amount=sc.nextInt();
@@ -53,8 +54,8 @@ public class Main {
 
         Transaction transaction =new Transaction(date,amount,category);
         }
+        Transaction.getTotalTransaction();
 
-        System.out.println(Transaction.totalTransactions);
 
     }
 
--- Transaction.java
+++ Transaction.java
@@ -4,12 +4,19 @@ public class Transaction {
     String category;
 
     static int totalTransactions=0;
+    static int totalNumberOfTransactions=0;
 
     public Transaction(String date,int amount,String category){
         this.date=date;
         this.amount=amount;
         this.category=category;
         totalTransactions+=amount;
+        totalNumberOfTransactions++;
+    }
+
+    public static void getTotalTransaction(){
+        System.out.println(totalTransactions);
+        System.out.println(totalNumberOfTransactions);
     }
 
 
